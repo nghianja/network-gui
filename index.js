d5d23e9387d70288013e9ef8e202f26250eb26e3
Joshua Lee <me@jolly.sg>
2017-01-24 12:49:29 +0800

    made buttons clickable

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+// buttons
+$('#startButton').on('click', function () {
+    pointIndex = 0;
+    $('#ex1').slider('setValue', pointIndex);
+    $("#ex1SliderVal").text(pointIndex);
+    highlightPointOnCharts();
+});
+$('#endButton').on('click', function () {
+    pointIndex = 9;
+    $('#ex1').slider('setValue', pointIndex);
+    $("#ex1SliderVal").text(pointIndex);
+    highlightPointOnCharts();
+});
+var isPlaying = false;
+$('#playPauseButton').on('click', function () {
+    if (isPlaying) {
+        isPlaying = false;
+        $('#playPauseIcon').removeClass('glyphicon-pause').addClass('glyphicon-play');
+    } else {
+        isPlaying = true;
+        $('#playPauseIcon').removeClass('glyphicon-play').addClass('glyphicon-pause');
+    }
+});
+
+// slider
 $('#ex1').slider({
     formatter: function(value) {
         return 'Current time: ' + value;
@@ -5,6 +30,9 @@ $('#ex1').slider({
 });
 var previousIndex = 0;
 var pointIndex = 0;
+$(document).ready(function() {
+    highlightPointOnCharts();
+});
 $("#ex1").on("slide", function(slideEvt) {
     $("#ex1SliderVal").text(slideEvt.value);
     pointIndex = slideEvt.value;
@@ -55,46 +83,6 @@ var eles = cy.add([
     { group: "edges", data: { id: "e1", source: "n0", target: "n2" } },
     { group: "edges", data: { id: "e2", source: "n1", target: "n2" } }
 ]);
-
-// var canvas1 = document.getElementById("chart1");
-// canvas1.width = parseInt($('#well1').css('width'), 10);
-// canvas1.height = parseInt($('#well1').css('height'), 10);
-// var chart1 = new Chart(canvas1, {
-//   type: 'bar',
-//   data: {
-//     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-//     datasets: [{
-//       label: '# of Votes',
-//       data: [12, 19, 3, 5, 2, 3],
-//       backgroundColor: [
-//         'rgba(255, 99, 132, 0.2)',
-//         'rgba(54, 162, 235, 0.2)',
-//         'rgba(255, 206, 86, 0.2)',
-//         'rgba(75, 192, 192, 0.2)',
-//         'rgba(153, 102, 255, 0.2)',
-//         'rgba(255, 159, 64, 0.2)'
-//       ],
-//       borderColor: [
-//         'rgba(255,99,132,1)',
-//         'rgba(54, 162, 235, 1)',
-//         'rgba(255, 206, 86, 1)',
-//         'rgba(75, 192, 192, 1)',
-//         'rgba(153, 102, 255, 1)',
-//         'rgba(255, 159, 64, 1)'
-//       ],
-//       borderWidth: 1
-//     }]
-//   },
-//   options: {
-//     scales: {
-//       yAxes: [{
-//         ticks: {
-//           beginAtZero:true
-//         }
-//       }]
-//     }
-//   }
-// });
 
 var cpuCanvas = document.getElementById("cpu-chart");
 cpuCanvas.width = parseInt($('#well1').css('width'), 10);
@@ -159,40 +147,3 @@ var networkChart = new Chart(networkCanvas, {
         }]
     }
 });
-
-// var data = {
-//     labels: [
-//         "Red",
-//         "Blue",
-//         "Yellow"
-//     ],
-//     datasets: [{
-//         data: [300, 50, 100],
-//         backgroundColor: [
-//             "#FF6384",
-//             "#36A2EB",
-//             "#FFCE56"
-//         ],
-//         hoverBackgroundColor: [
-//             "#FF6384",
-//             "#36A2EB",
-//             "#FFCE56"
-//         ]
-//     }]
-// };
-
-// var canvas3 = document.getElementById("chart3");
-// canvas3.width = parseInt($('#well3').css('width'), 10);
-// canvas3.height = parseInt($('#well3').css('height'), 10);
-// var chart3 = new Chart(canvas3, {
-//   type: 'pie',
-//   data: data
-// });
-//
-// var canvas4 = document.getElementById("chart4");
-// canvas4.width = parseInt($('#well4').css('width'), 10);
-// canvas4.height = parseInt($('#well4').css('height'), 10);
-// var chart4 = new Chart(canvas4, {
-//   type: 'doughnut',
-//   data: data
-// });
