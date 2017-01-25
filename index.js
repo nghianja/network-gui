ecb53c9c4b2664e7042dacfc1a9e410556178c17
James Ng <jng@ncl.sg>
2017-01-25 14:08:11 +0800

    Update topology playback
    1. Add colors to nodes and edges for usage loads
    2. Resolve event trigger upon slider click

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ $('#startButton').on('click', function () {
     pointIndex = 0;
     $('#ex1').slider('setValue', pointIndex);
     $("#ex1SliderVal").text(pointIndex);
+    highlightNetworkLoad();
     highlightPointOnCharts();
 });
 $('#endButton').on('click', function () {
     pointIndex = 9;
     $('#ex1').slider('setValue', pointIndex);
     $("#ex1SliderVal").text(pointIndex);
+    highlightNetworkLoad();
     highlightPointOnCharts();
 });
 var isPlaying = false;
@@ -27,6 +29,7 @@ function playbackTicker() {
         pointIndex++;
         $('#ex1').slider('setValue', pointIndex);
         $("#ex1SliderVal").text(pointIndex);
+        highlightNetworkLoad();
         highlightPointOnCharts();
         window.setTimeout(playbackTicker, 500);
     } else if (isPlaying && pointIndex >= 9) {
@@ -44,13 +47,16 @@ $('#ex1').slider({
 var previousIndex = 0;
 var pointIndex = 0;
 $(document).ready(function() {
+    highlightNetworkLoad();
     highlightPointOnCharts();
 });
-$("#ex1").on("slide", function(slideEvt) {
+$("#ex1").on("slideStop", function(slideEvt) {
     $("#ex1SliderVal").text(slideEvt.value);
     pointIndex = slideEvt.value;
+    highlightNetworkLoad();
     highlightPointOnCharts();
 });
+
 function highlightPointOnCharts(){
     var cpuMeta = cpuChart.getDatasetMeta(0);
     var networkMeta = networkChart.getDatasetMeta(0);
@@ -82,6 +88,20 @@ function highlightPointOnCharts(){
     networkChart.update(0);
 
     previousIndex = pointIndex;
+}
+function highlightNetworkLoad() {
+    var cpuData = [10, 65, 59, 80, 20, 81, 56, 55, 30, 40];
+    var networkData = [50, 50, 50, 50, 40, 30, 70, 90, 100, 100];
+    if (cpuData[pointIndex] > 50) {
+        cy.nodes().style({ 'background-color':'red' });
+    } else {
+        cy.nodes().style({ 'background-color':'green' });
+    }
+    if (networkData[pointIndex] > 50) {
+        cy.edges().style({ 'line-color':'red' });
+    } else {
+        cy.edges().style({ 'line-color':'green' });
+    }
 }
 
 var cy = cytoscape({
--- main.js
+++ main.js
@@ -14,7 +14,6 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
-  // mainWindow = new BrowserWindow({width: 1400, height: 600})
   mainWindow.setMenu(null)
 
   // and load the index.html of the app.
