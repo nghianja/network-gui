f1354916835080439b95e49d2602a2f9925867f6
teye <a0097953@gmail.com>
2017-02-07 11:27:16 +0800

    remove redundant classes

--- data_topology.js
+++ data_topology.js
@@ -1,22 +1,22 @@
 // TODO: refactor to for loop
 dataset = [
         // central switch
-        { data: { id: 'z', label: 'switch' }, classes: 'bottom-center switch' },
+        { data: { id: 'z', label: 'switch' }, classes: 'switch' },
         // group 1
-        { data: { id: 'a', label: '192.168.1.1' }, classes: 'bottom-center' },
-        { data: { id: 'b', label: '192.168.1.2' }, classes: 'bottom-center' },
-        { data: { id: 'c', label: '192.168.1.3' }, classes: 'bottom-center' },
-        { data: { id: 'd', label: 'switch' }, classes: 'bottom-center switch' },
+        { data: { id: 'a', label: '192.168.1.1' }},
+        { data: { id: 'b', label: '192.168.1.2' }},
+        { data: { id: 'c', label: '192.168.1.3' }},
+        { data: { id: 'd', label: 'switch' }, classes: 'switch' },
         // group 2
-        { data: { id: 'e', label: '192.168.2.1' }, classes: 'bottom-center' },
-        { data: { id: 'f', label: '192.168.2.2' }, classes: 'bottom-center' },
-        { data: { id: 'g', label: '192.168.2.3' }, classes: 'bottom-center' },
-        { data: { id: 'h', label: 'switch' }, classes: 'bottom-center switch' },
+        { data: { id: 'e', label: '192.168.2.1' }},
+        { data: { id: 'f', label: '192.168.2.2' }},
+        { data: { id: 'g', label: '192.168.2.3' }},
+        { data: { id: 'h', label: 'switch' }, classes: 'switch' },
         // group 3
-        { data: { id: 'i', label: '192.168.3.1' }, classes: 'bottom-center' },
-        { data: { id: 'j', label: '192.168.3.2' }, classes: 'bottom-center' },
-        { data: { id: 'k', label: '192.168.3.3' }, classes: 'bottom-center' },
-        { data: { id: 'l', label: 'switch' }, classes: 'bottom-center switch' },
+        { data: { id: 'i', label: '192.168.3.1' }},
+        { data: { id: 'j', label: '192.168.3.2' }},
+        { data: { id: 'k', label: '192.168.3.3' }},
+        { data: { id: 'l', label: 'switch' }, classes: 'switch' },
         // edges for group 1
         { data: { id: 'ad', source: 'a', target: 'd' } },
         { data: { id: 'bd', source: 'b', target: 'd' } },
