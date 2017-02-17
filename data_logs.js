389e69f1c1bdc8fb1c23bb399172c0288ca8d0eb
Joshua Lee <me@jolly.sg>
2017-02-17 11:20:51 +0800

    read all logs

--- data_logs.js
+++ data_logs.js
@@ -1,8 +1,15 @@
+let fs = require('fs');
 let logs = require('./logs');
+let path = require('path');
 const {ipcRenderer} = require('electron');
 
-module.exports.readFiles = function() {
+module.exports.readFiles = function(dir) {
     ipcRenderer.send('main', 'reading logs');
-    logs.readFile('anode-0');
-    logs.readFile('anode-1');
+    fs.readdir(path.join(__dirname, '/logs_output'), (err, dir) => {
+        ipcRenderer.send('main', 'number of files: ' + dir.length);
+        for (i = 0, path; path = dir[i]; i++) {
+            // ipcRenderer.send('main', path);
+            logs.readFile(path)
+        }
+    });
 };
