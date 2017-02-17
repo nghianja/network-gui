32e07e8516e7c0b0cd3b3327f6d5c66c09a5397e
Joshua Lee <me@jolly.sg>
2017-02-17 11:01:41 +0800

    some parsing of logs

--- logs.js
+++ logs.js
@@ -8,6 +8,38 @@ module.exports.readFile = function() {
         if (err) return console.log(err);
 
         // data is the contents of the text file we just read
-        ipcRenderer.send('main', data);
+        // ipcRenderer.send('main', data);
+        parseLog(data);
     });
 };
+
+function parseLog(data) {
+    var lines = data.split(/\s+/g);
+    var lastLineIsEmpty = false;
+    if (lines[lines.length - 1].length == 0) {
+        lastLineIsEmpty = true;
+        // ipcRenderer.send('main', 'input has ' + (lines.length - 1) + ' lines of data');
+    }
+    ipcRenderer.send('main', 'node: ' + lines[0]);
+    var numOfPorts = 0;
+    var endLine = 0;
+    if (lastLineIsEmpty) {
+        numOfPorts = (lines.length - 2) / 2;
+        endLine = lines.length - 1;
+    } else {
+        numOfPorts = (lines.length - 1) / 2;
+        endLine = lines.length;
+    }
+    ipcRenderer.send('main', 'number of ports: ' + numOfPorts);
+    for(i = 1; i < endLine; i++){
+        var parts = lines[i].split(',');
+        // ipcRenderer.send('main', 'number of parts: ' + parts.length);
+        ipcRenderer.send('main', 'port: ' + parts[0].substring(0, 4));
+        if (parts[0].endsWith('inp')) {
+            ipcRenderer.send('main', 'type of traffic: input');
+        } else {
+            ipcRenderer.send('main', 'type of traffic: output');
+        }
+        ipcRenderer.send('main', 'throughput: ' + parts.slice(1));
+    }
+}
