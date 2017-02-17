d08d1ca3b7332900650678ac7cd07e45020ae14c
Joshua Lee <me@jolly.sg>
2017-02-17 14:27:28 +0800

    removed extra port charts

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const {ipcRenderer} = require('electron');
 // load dummy data
 require('./data_nodes');
 
@@ -83,29 +84,16 @@ function updateDataSources() {
 function update() {
     updateHighlights();
     updateLabels();
-    updateConsole();
 }
 
 function updateHighlights() {
-    highlightPointOnCharts();
+    // highlightPointOnCharts();
     highlightOverallNetworkLoad();
-    highlightNetworkLoad();
+    // highlightNetworkLoad();
 }
 
 function updateLabels() {
     $("#currentTimeLabel").text(pointIndex);
-}
-
-const {ipcRenderer} = require('electron')
-function updateConsole() {
-    if (currentNode != numberOfNodes) {
-        ipcRenderer.send('main', "CPU load: " + cpuData[currentNode][pointIndex]);
-        for (i = 0; i < numberOfPorts[currentNode]; i++) {
-            ipcRenderer.send('main', "Throughput: " + throughputData[currentNode][i][pointIndex]);
-            ipcRenderer.send('main', "Latency: " + latencyData[currentNode][i][pointIndex]);
-            ipcRenderer.send('main', "Packets Dropped: " + dropPacketData[currentNode][i][pointIndex]);
-        }
-    }
 }
 
 function highlightPointOnCharts() {
@@ -384,19 +372,19 @@ var cy = cytoscape({
         }
     ]
 });
-cy.on('mouseover', 'node', function(event) {
-    var node = event.cyTarget;
-    node.qtip({
-         content: this.data('label'),
-         show: {
-            event: event.type,
-            ready: true
-         },
-         hide: {
-            event: 'mouseout unfocus'
-         }
-    }, event);
-});
+// cy.on('mouseover', 'node', function(event) {
+//     var node = event.cyTarget;
+//     node.qtip({
+//          content: this.data('label'),
+//          show: {
+//             event: event.type,
+//             ready: true
+//          },
+//          hide: {
+//             event: 'mouseout unfocus'
+//          }
+//     }, event);
+// });
 cy.on('click', function(event) {
     var node = event.cyTarget;
     if (node === cy) {
@@ -458,327 +446,6 @@ var portChart2 = new Chart(portCanvas2, {
     }
 });
 
-var portCanvas3 = document.getElementById("port-chart3");
-portCanvas3.width = parseInt($('#well5').css('width'), 10);
-portCanvas3.height = parseInt($('#well5').css('height'), 10);
-var portChart3 = new Chart(portCanvas3, {
-    type: "line",
-    data: portChartStyle3,
-    options: {
-        title: {
-            display: true,
-            text: 'Port [2]'
-        },
-        scales: portChartScales
-    }
-});
-
-var portCanvas4 = document.getElementById("port-chart4");
-portCanvas4.width = parseInt($('#well6').css('width'), 10);
-portCanvas4.height = parseInt($('#well6').css('height'), 10);
-var portChart4 = new Chart(portCanvas4, {
-    type: "line",
-    data: portChartStyle4,
-    options: {
-        title: {
-            display: true,
-            text: 'Port [3]'
-        },
-        scales: portChartScales
-    }
-});
-
-var portCanvas5 = document.getElementById("port-chart5");
-portCanvas5.width = parseInt($('#well7').css('width'), 10);
-portCanvas5.height = parseInt($('#well7').css('height'), 10);
-var portChart5 = new Chart(portCanvas5, {
-    type: "line",
-    data: portChartStyle5,
-    options: {
-        title: {
-            display: true,
-            text: 'Port [4]'
-        },
-        scales: portChartScales
-    }
-});
-
-var portCanvas6 = document.getElementById("port-chart6");
-portCanvas6.width = parseInt($('#well8').css('width'), 10);
-portCanvas6.height = parseInt($('#well8').css('height'), 10);
-var portChart6 = new Chart(portCanvas6, {
-    type: "line",
-    data: portChartStyle6,
-    options: {
-        title: {
-            display: true,
-            text: 'Port [5]'
-        },
-        scales: portChartScales
-    }
-});
-
-var portCanvas7 = document.getElementById("port-chart7");
-portCanvas7.width = parseInt($('#well9').css('width'), 10);
-portCanvas7.height = parseInt($('#well9').css('height'), 10);
-var portChart7 = new Chart(portCanvas7, {
-    type: "line",
-    data: portChartStyle7,
-    options: {
-        title: {
-            display: true,
-            text: 'Port [6]'
-        },
-        scales: portChartScales
-    }
-});
-
-var portCanvas8 = document.getElementById("port-chart8");
-portCanvas8.width = parseInt($('#well10').css('width'), 10);
-portCanvas8.height = parseInt($('#well10').css('height'), 10);
-var portChart8 = new Chart(portCanvas8, {
-    type: "line",
-    data: portChartStyle8,
-    options: {
-        title: {
-            display: true,
-            text: 'Port [7]'
-        },
-        scales: portChartScales
-    }
-});
-
-var portCanvas9 = document.getElementById("port-chart9");
-portCanvas9.width = parseInt($('#well11').css('width'), 10);
-portCanvas9.height = parseInt($('#well11').css('height'), 10);
-var portChart9 = new Chart(portCanvas9, {
-    type: "line",
-    data: portChartStyle9,
-    options: {
-        title: {
-            display: true,
-            text: 'Port [8]'
-        },
-        scales: portChartScales
-    }
-});
-
-var portCanvas10 = document.getElementById("port-chart10");
-portCanvas10.width = parseInt($('#well12').css('width'), 10);
-portCanvas10.height = parseInt($('#well12').css('height'), 10);
-var portChart10 = new Chart(portCanvas10, {
-    type: "line",
-    data: portChartStyle10,
-    options: {
-        title: {
-            display: true,
-            text: 'Port [9]'
-        },
-        scales: portChartScales
-    }
-});
-
-var portCanvas11 = document.getElementById("port-chart11");
-portCanvas11.width = parseInt($('#well13').css('width'), 10);
-portCanvas11.height = parseInt($('#well13').css('height'), 10);
-var portChart11 = new Chart(portCanvas11, {
-    type: "line",
-    data: portChartStyle11,
-    options: {
-        title: {
-            display: true,
-            text: 'Port [10]'
-        },
-        scales: portChartScales
-    }
-});
-
-var portCanvas12 = document.getElementById("port-chart12");
-portCanvas12.width = parseInt($('#well14').css('width'), 10);
-portCanvas12.height = parseInt($('#well14').css('height'), 10);
-var portChart12 = new Chart(portCanvas12, {
-    type: "line",
-    data: portChartStyle12,
-    options: {
-        title: {
-            display: true,
-            text: 'Port [11]'
-        },
-        scales: portChartScales
-    }
-});
-
-var portCanvas13 = document.getElementById("port-chart13");
-portCanvas13.width = parseInt($('#well15').css('width'), 10);
-portCanvas13.height = parseInt($('#well15').css('height'), 10);
-var portChart13 = new Chart(portCanvas13, {
-    type: "line",
-    data: portChartStyle13,
-    options: {
-        title: {
-            display: true,
-            text: 'Port [12]'
-        },
-        scales: portChartScales
-    }
-});
-
-var portCanvas14 = document.getElementById("port-chart14");
-portCanvas14.width = parseInt($('#well16').css('width'), 10);
-portCanvas14.height = parseInt($('#well16').css('height'), 10);
-var portChart14 = new Chart(portCanvas14, {
-    type: "line",
-    data: portChartStyle14,
-    options: {
-        title: {
-            display: true,
-            text: 'Port [13]'
-        },
-        scales: portChartScales
-    }
-});
-
-var portCanvas15 = document.getElementById("port-chart15");
-portCanvas15.width = parseInt($('#well17').css('width'), 10);
-portCanvas15.height = parseInt($('#well17').css('height'), 10);
-var portChart15 = new Chart(portCanvas15, {
-    type: "line",
-    data: portChartStyle15,
-    options: {
-        title: {
-            display: true,
-            text: 'Port [14]'
-        },
-        scales: portChartScales
-    }
-});
-
-
-var portCanvas16 = document.getElementById("port-chart16");
-portCanvas16.width = parseInt($('#well18').css('width'), 10);
-portCanvas16.height = parseInt($('#well18').css('height'), 10);
-var portChart16 = new Chart(portCanvas16, {
-    type: "line",
-    data: portChartStyle16,
-    options: {
-        title: {
-            display: true,
-            text: 'Port [15]'
-        },
-        scales: portChartScales
-    }
-});
-
-var portCanvas17 = document.getElementById("port-chart17");
-portCanvas17.width = parseInt($('#well19').css('width'), 10);
-portCanvas17.height = parseInt($('#well19').css('height'), 10);
-var portChart17 = new Chart(portCanvas17, {
-    type: "line",
-    data: portChartStyle17,
-    options: {
-        title: {
-            display: true,
-            text: 'Port [16]'
-        },
-        scales: portChartScales
-    }
-});
-
-var portCanvas18 = document.getElementById("port-chart18");
-portCanvas18.width = parseInt($('#well20').css('width'), 10);
-portCanvas18.height = parseInt($('#well20').css('height'), 10);
-var portChart18 = new Chart(portCanvas18, {
-    type: "line",
-    data: portChartStyle18,
-    options: {
-        title: {
-            display: true,
-            text: 'Port [17]'
-        },
-        scales: portChartScales
-    }
-});
-
-var portCanvas19 = document.getElementById("port-chart19");
-portCanvas19.width = parseInt($('#well21').css('width'), 10);
-portCanvas19.height = parseInt($('#well21').css('height'), 10);
-var portChart19 = new Chart(portCanvas19, {
-    type: "line",
-    data: portChartStyle19,
-    options: {
-        title: {
-            display: true,
-            text: 'Port [18]'
-        },
-        scales: portChartScales
-    }
-});
-
-var portCanvas20 = document.getElementById("port-chart20");
-portCanvas20.width = parseInt($('#well22').css('width'), 10);
-portCanvas20.height = parseInt($('#well22').css('height'), 10);
-var portChart20 = new Chart(portCanvas20, {
-    type: "line",
-    data: portChartStyle20,
-    options: {
-        title: {
-            display: true,
-            text: 'Port [19]'
-        },
-        scales: portChartScales
-    }
-});
-
-var portCanvas21 = document.getElementById("port-chart21");
-portCanvas21.width = parseInt($('#well23').css('width'), 10);
-portCanvas21.height = parseInt($('#well23').css('height'), 10);
-var portChart21 = new Chart(portCanvas21, {
-    type: "line",
-    data: portChartStyle21,
-    options: {
-        title: {
-            display: true,
-            text: 'Port [20]'
-        },
-        scales: portChartScales
-    }
-});
-
-var portCanvas22 = document.getElementById("port-chart22");
-portCanvas22.width = parseInt($('#well24').css('width'), 10);
-portCanvas22.height = parseInt($('#well24').css('height'), 10);
-var portChart22 = new Chart(portCanvas22, {
-    type: "line",
-    data: portChartStyle22,
-    options: {
-        title: {
-            display: true,
-            text: 'Port [21]'
-        },
-        scales: portChartScales
-    }
-});
-
 portCharts = [
     portChart1,
-    portChart2,
-    portChart3,
-    portChart4,
-    portChart5,
-    portChart6,
-    portChart7,
-    portChart8,
-    portChart9,
-    portChart10,
-    portChart11,
-    portChart12,
-    portChart13,
-    portChart14,
-    portChart15,
-    portChart16,
-    portChart17,
-    portChart18,
-    portChart19,
-    portChart20,
-    portChart21,
-    portChart22];
+    portChart2];
